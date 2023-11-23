6370a169c9bfcde33b1c3074e4643ac2a9f675dd
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-11-23 14:26:54 +0530

    added react- router for the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Home from './component/Home/Home';
+import MovieDetail from './component/MovieDetail/MovieDetail'
+import PageNotFound from './component/PageNotFound/PageNotFound'
+import Header from './component/Header/Header'
+import Footer from './component/Footer/Footer'
 
 function App() {
   return (
-    <div className="App">
-     APP
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path= '/' element={<Home />} />
+        <Route path= '/movie/:imdbID' element={<MovieDetail />} />
+        <Route path= '/movie/:imdbID' element={<MovieDetail />} />
+        <Route path= '*' element={<PageNotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
