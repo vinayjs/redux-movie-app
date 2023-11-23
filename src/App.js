56b7035afb80eff3cfa596b5caeaf0d818af3ac4
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-11-23 15:38:47 +0530

    added css for App - common styling

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <div className='container'>
       <Routes>
         <Route path= '/' element={<Home />} />
         <Route path= '/movie/:imdbID' element={<MovieDetail />} />
         <Route path= '/movie/:imdbID' element={<MovieDetail />} />
         <Route path= '*' element={<PageNotFound />} />
       </Routes>
+      </div>
       <Footer />
     </div>
   );
